2887cc3d3ebe7cfb4bba22dc6c099a69016edb8f
Marshall Ward <marshall.ward@noaa.gov>
2025-05-23 15:02:26 -0400

    Loop with function pointer
    does not seem to affect performance

--- run_conc_order.f90
+++ run_conc_order.f90
@@ -2,25 +2,35 @@ use, intrinsic :: iso_fortran_env, only : int64
 
 use do_conc_order
 
+interface
+  subroutine do_loop_test(a,b,c,d)
+    real, intent(in) :: a(:,:), b(:,:), c(:,:)
+    real, intent(out) :: d(:,:)
+  end subroutine
+end interface
+
+! 1024 x 4096 arrays
 integer, parameter :: m = 2**10, n = 2**12
 real :: a(m,n), b(m,n), c(m,n), d(m,n)
 
 integer(int64) :: c_start, c_end, c_rate, c_max
-real :: t
 real, parameter :: t_max = 1.
+real :: t
 
 integer :: r
 integer :: n_reps
 
-! Clock setup
-call system_clock(count_rate=c_rate, count_max=c_max)
+procedure(do_loop_test), pointer :: test => null()
+integer :: p
 
 a(:,:) = 1.1
 b(:,:) = 2.2
 c(:,:) = 3.3
 
+! Clock setup
+call system_clock(count_rate=c_rate, count_max=c_max)
+
 ! Force the CPU into the highest frequency step
-! TODO: function pointers would help out here
 
 n_reps = 1
 t = 0.
@@ -36,74 +46,30 @@ do while (t < t_max)
   if (t < t_max) n_reps = n_reps * 2
 end do
 
-! Generate do concurrent (i=, j=) timings
+! Run tests
+do p=1,4
+  if (p == 1) test => do_conc_ij
+  if (p == 2) test => do_ij
+  if (p == 3) test => do_conc_ji
+  if (p == 4) test => do_ji
 
-n_reps = 1
-t = 0.
-do while (t < t_max)
-  call system_clock(c_start)
-  do r = 1, n_reps
-    call do_conc_ij(a,b,c,d)
+  n_reps = 1
+  t = 0.
+  do while (t < t_max)
+    call system_clock(c_start)
+    do r = 1, n_reps
+      call test(a,b,c,d)
+    end do
+    call system_clock(c_end)
+
+    t = real(c_end - c_start) / real(c_rate)
+    if (t < t_max) n_reps = n_reps * 2
   end do
-  call system_clock(c_end)
 
-  t = real(c_end - c_start) / real(c_rate)
-  if (t < t_max) n_reps = n_reps * 2
+  if (p == 1) print '("do concurrent(i=, j=) ", es24.16)', t / n_reps
+  if (p == 2) print '("do i ; do j ", es24.16)', t / n_reps
+  if (p == 3) print '("do concurrent (j=, i=) ", es24.16)', t / n_reps
+  if (p == 4) print '("do j ; do i ", es24.16)', t / n_reps
 end do
-print '("do concurrent(i=, j=) ", es24.16)', &
-    real(c_end - c_start) / real(c_rate) / n_reps
-
-! Generate do (i=, j=) timings
-
-n_reps = 1
-t = 0.
-do while (t < t_max)
-  call system_clock(c_start)
-  do r = 1, n_reps
-    call do_ij(a,b,c,d)
-  end do
-  call system_clock(c_end)
-
-  t = real(c_end - c_start) / real(c_rate)
-  if (t < t_max) n_reps = n_reps * 2
-end do
-print '("do (i=, j=) ", es24.16)', &
-    real(c_end - c_start) / real(c_rate) / n_reps
-
-! Generate do concurrent (j=, i=) timings
-
-n_reps = 1
-t = 0.
-do while (t < t_max)
-  call system_clock(c_start)
-  do r = 1, n_reps
-    call do_conc_ji(a,b,c,d)
-  end do
-  call system_clock(c_end)
-
-  t = real(c_end - c_start) / real(c_rate)
-  if (t < t_max) n_reps = n_reps * 2
-end do
-
-print '("do concurrent(j=, i=) ", es24.16)', &
-    real(c_end - c_start) / real(c_rate) / n_reps
-
-! Generate do (j=, i=) timings
-
-n_reps = 1
-t = 0.
-do while (t < t_max)
-  call system_clock(c_start)
-  do r = 1, n_reps
-    call do_ji(a,b,c,d)
-  end do
-  call system_clock(c_end)
-
-  t = real(c_end - c_start) / real(c_rate)
-  if (t < t_max) n_reps = n_reps * 2
-end do
-
-print '("do (j=, i=) ", es24.16)', &
-    real(c_end - c_start) / real(c_rate) / n_reps
 
 end
